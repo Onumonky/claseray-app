6375369232921e3a4888f44c01e8848ad53330c7
Ray Parra <rparragalaviz@gmail.com>
2025-05-30 20:57:58 -0700

    First-Strike BANK CRUD

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,7 @@ app_name = "core"
 
 
 urlpatterns = [
-    
+    path('list/banks/', views.ListBank.as_view(), name="list_bank"),
+    path('create/banks/', views.CreateBank.as_view(), name="create_bank"),
+    path('detail/bank/<int:pk>/', views.DetailBank.as_view(), name="detail_bank"),
 ]
