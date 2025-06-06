4008d328aafc2b325287176a972159100e81968f
Ray Parra <rparragalaviz@gmail.com>
2025-06-05 20:11:23 -0700

    BANK CRUD update - delete

--- core/forms.py
+++ core/forms.py
@@ -15,3 +15,17 @@ class CreateBankForm(forms.ModelForm):
             "bank_name": forms.TextInput(attrs={"type": "text", "class": "form-control", "placeholder": "Escribe el nombre del Banco!"}),
             "address": forms.TextInput(attrs={"type": "text", "class": "form-control", "placeholder": "Escribe la direccion del Banco!"}),
         }
+
+
+class UpdateBankForm(forms.ModelForm):
+    class Meta:
+        model = models.Bank
+        fields = [
+            "bank_name",
+            "address",
+            "status",
+        ]
+        widgets = {
+            "bank_name": forms.TextInput(attrs={"type": "text", "class": "form-control", "placeholder": "Escribe el nombre del Banco!"}),
+            "address": forms.TextInput(attrs={"type": "text", "class": "form-control", "placeholder": "Escribe la direccion del Banco!"}),
+        }
